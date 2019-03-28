b541b902e4fb005d1f752627ca8f7982ad4c56b3
Markku-Juhani O. Saarinen <mjos@iki.fi>
2019-03-28 10:04:04 +0000

    update

--- stm32f4/codesize.sh
+++ stm32f4/codesize.sh
@@ -2,5 +2,8 @@
 
 # this is a god awful hack
 
-echo "ibase=16;" `objdump -x r5test.elf | grep text | grep -e "r5_" -e "crypto_" -e "xe" | colrm 1 24 | colrm 9 | tr 'a-f\n' 'A-F+'` 0 | bc
+echo "ibase=16;" `arm-none-eabi-objdump -x r5test.elf | grep "F .text" | \
+	grep -e "r5_" -e "crypto_" -e "xe" | \
+	colrm 1 24 | colrm 9 | tr 'a-f\n' 'A-F+'` 0 | bc
 
+# (sums up sizes of functions with "r5_", "crypto_", or "xe" in their name)
